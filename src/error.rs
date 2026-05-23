942d40ab97840d9a6699d7528fa0c12f7aad4c15
Jiwon <rkdwldnjs878@gmail.com>
2026-05-24 04:18:06 +0900

    refactor: drop unused ApiError::BadRequest variant

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,7 @@
 use axum::{
+    Json,
     http::StatusCode,
     response::{IntoResponse, Response},
-    Json,
 };
 use serde_json::json;
 use thiserror::Error;
@@ -10,9 +10,6 @@ pub type ApiResult<T> = Result<T, ApiError>;
 
 #[derive(Debug, Error)]
 pub enum ApiError {
-    #[error("{0}")]
-    BadRequest(String),
-
     #[error("{0}")]
     NotFound(String),
 
@@ -38,7 +35,6 @@ impl From<sqlx::Error> for ApiError {
 impl IntoResponse for ApiError {
     fn into_response(self) -> Response {
         let (status, message) = match &self {
-            ApiError::BadRequest(msg) => (StatusCode::BAD_REQUEST, msg.clone()),
             ApiError::NotFound(msg) => (StatusCode::NOT_FOUND, msg.clone()),
             ApiError::Conflict(msg) => (StatusCode::CONFLICT, msg.clone()),
             ApiError::Internal(err) => {
